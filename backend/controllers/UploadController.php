80a025bca9ed48c980facef30ef766ae4b1ce9f5
quyettvq <tvq16091993tvq>
2017-03-31 03:40:39 +0700

    behavior

--- backend/controllers/UploadController.php
+++ backend/controllers/UploadController.php
@@ -23,23 +23,22 @@ class UploadController extends Controller
             $model->imageFiles = UploadedFile::getInstances($model, 'imageFiles');
             if ($images = $model->uploadImages()) {
                 // file is uploaded successfully
-                $saved = '<ul>';
-                foreach ($images['saved'] as $image) {
-                    $saved .= "<li><a href='{$image->getSource()}' target='_blank'>$image->name</a></li>";
-                }
-                $saved .= '</ul>';
-
-                $unsaved = '<ul>';
-                foreach ($images['unsaved'] as $image) {
-                    $unsaved .= "<li><a href='{$image->getSource()}' target='_blank'>$image->name</a></li>";
-                }
-                $unsaved .= '</ul>';
 
                 if (count($images['saved']) > 0) {
+                    $saved = '<ul>';
+                    foreach ($images['saved'] as $image) {
+                        $saved .= "<li><a href='{$image->getSource()}' target='_blank'>$image->name</a></li>";
+                    }
+                    $saved .= '</ul>';
                     Yii::$app->session->setFlash('success', '<div>Uploaded Successfully:</div>' . $saved);
                 }
 
                 if (count($images['unsaved']) > 0) {
+                    $unsaved = '<ul>';
+                    foreach ($images['unsaved'] as $image) {
+                        $unsaved .= "<li><a href='{$image->getSource()}' target='_blank'>$image->name</a></li>";
+                    }
+                    $unsaved .= '</ul>';
                     Yii::$app->session->setFlash('error', '<div>Uploaded fail:</div>' . $unsaved);
                 }
             }
