981679f912cd15069ae2daebb3e51d97717b5469
quyettvq <tvq16091993tvq>
2017-04-02 17:31:35 +0700

    image

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,6 +5,12 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@images', dirname(dirname(__DIR__)) . '/frontend/web/images');
 
-Yii::setAlias('@frontendUrl', '//localhost/quyettran.com/frontend/web');
-Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend/web');
-Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/frontend/web/images');
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    Yii::setAlias('@frontendUrl', '//localhost/quyettran.com/frontend/web');
+    Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend/web');
+    Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/frontend/web/images');
+} else {
+    Yii::setAlias('@frontendUrl', $_SERVER['HTTP_HOST']);
+    Yii::setAlias('@backendUrl', $_SERVER['HTTP_HOST'] . '/backend');
+    Yii::setAlias('@imagesUrl', $_SERVER['HTTP_HOST'] . '/images');
+}
--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'frontendUrlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [],
+        ],
+        'backendUrlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [],
+        ],
     ],
 ];
