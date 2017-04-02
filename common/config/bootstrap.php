0b901071f515abda8302c5dbbbe9930b18faa167
quyettvq <tvq16091993tvq>
2017-04-02 17:32:00 +0700

    image

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -6,9 +6,9 @@ Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@images', dirname(dirname(__DIR__)) . '/frontend/web/images');
 
 if ($_SERVER['HTTP_HOST'] == 'localhost') {
-    Yii::setAlias('@frontendUrl', '//localhost/quyettran.com/frontend/web');
-    Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend/web');
-    Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/frontend/web/images');
+    Yii::setAlias('@frontendUrl', '//localhost/quyettran.com');
+    Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend');
+    Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/images');
 } else {
     Yii::setAlias('@frontendUrl', $_SERVER['HTTP_HOST']);
     Yii::setAlias('@backendUrl', $_SERVER['HTTP_HOST'] . '/backend');
