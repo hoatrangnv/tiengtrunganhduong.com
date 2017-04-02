70b9e5906291a4d94a011e543696b298ac88d5cc
quyettvq <tvq16091993tvq>
2017-04-02 17:39:19 +0700

    image

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,13 +5,13 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@images', dirname(dirname(__DIR__)) . '/frontend/web/images');
 
-if ($_SERVER['HTTP_HOST'] == 'localhost') {
-    Yii::setAlias('@frontendUrl', '//localhost/quyettran.com');
-    Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend');
-    Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/images');
-} else {
+if ($_SERVER['HTTP_HOST'] != 'localhost') {
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://';
     Yii::setAlias('@frontendUrl', $protocol . $_SERVER['HTTP_HOST']);
     Yii::setAlias('@backendUrl', $protocol . $_SERVER['HTTP_HOST'] . '/backend');
     Yii::setAlias('@imagesUrl', $protocol . $_SERVER['HTTP_HOST'] . '/images');
+} else {
+    Yii::setAlias('@frontendUrl', '//localhost/quyettran.com');
+    Yii::setAlias('@backendUrl', '//localhost/quyettran.com/backend');
+    Yii::setAlias('@imagesUrl', '//localhost/quyettran.com/images');
 }
