32d5131d2a5fea064035b23f0b74c7c565640944
quyettvq <tvq16091993tvq>
2017-04-06 01:08:42 +0700

    site index

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -39,7 +39,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= Yii::$app->name ?> <?= date('Y') ?></p>
     </div>
 </footer>
 
