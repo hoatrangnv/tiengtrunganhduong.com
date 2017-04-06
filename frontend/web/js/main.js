e14351618bd0679145a478b1f1b3c4e9188a6f49
quyettvq <tvq16091993tvq>
2017-04-06 23:09:29 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -24,7 +24,7 @@
         code_example.contentEditable = true;
 
         code_example.onkeydown = function (event) {
-            code_example.innerHTML = htmlEntitiesDecode(code_example.innerHTML);
+            code_example.innerHTML = htmlEntitiesEncode(code_example.innerHTML);
             var tab = "    "; // 1 tab ===> 4 space
             code_example.innerHTML.split("\t").join(tab);
 
@@ -76,7 +76,6 @@
                 document.execCommand("insertHTML", false, tab);
                 return false;
             }
-            code_example.innerHTML = htmlEntitiesEncode(code_example.innerHTML);
         };
     });
 }(document.querySelectorAll(".code-example"));
