be36c90094159e61649144fc1a1d25da1e8cbdf5
quyettvq <tvq16091993tvq>
2017-04-06 17:21:47 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -5,12 +5,21 @@
 !function (code_examples) {
     [].forEach.call(code_examples, function (code_example) {
         var test_block = document.createElement("DIV");
-        test_block.innerHTML = code_example.innerHTML;
 
         if (code_example.nextSibling) {
             code_example.parentNode.insertBefore(test_block, code_example.nextSibling);
         } else {
             code_example.parentNode.appendChild(test_block);
         }
+
+        var btn = document.createElement("BUTTON");
+        btn.innerHTML = "Try it here";
+        btn.addEventListener("click", function (event) {
+            test_block.innerHTML = code_example.innerHTML;
+        }, false);
+        code_example.parentNode.insertBefore(btn, test_block);
+        btn.click();
+
+        code_example.contentEditable = true;
     });
 }(document.querySelectorAll(".code-example"));
