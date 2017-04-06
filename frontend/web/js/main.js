50a8649d22a8f3b04a70bf7588f18228776afbd4
quyettvq <tvq16091993tvq>
2017-04-07 00:45:59 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -78,13 +78,13 @@
                         white_space += tab;
                     }
                     document.execCommand("insertHTML", false, "\n" + white_space);
-                    if ( code_example.innerHTML.slice(-1) === "\n"
-                      && code_example.innerHTML.slice(-2, -1) !== "\n"
-                      && code.length == getCaretOffset(code_example)
-                    ) {
-                        // Ensure break line
-                        document.execCommand("insertHTML", false, "\n" + white_space);
-                    }
+                    // if ( code_example.innerHTML.slice(-1) === "\n"
+                    //   && code_example.innerHTML.slice(-2, -1) !== "\n"
+                    //   && code.length == getCaretOffset(code_example)
+                    // ) {
+                    //     // Ensure break line
+                    //     document.execCommand("insertHTML", false, "\n" + white_space);
+                    // }
                 }
 
                 return false;
