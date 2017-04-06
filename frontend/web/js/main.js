95d2db4463dcab487f464f7786d3545c0e65f050
quyettvq <tvq16091993tvq>
2017-04-06 23:52:28 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -111,5 +111,6 @@ function getCaretCharacterOffsetWithin(element) {
         caretOffset = preCaretTextRange.text.length;
     }
     var left_str = htmlEntitiesDecode(element.innerHTML.substring(0, caretOffset - 1));
+    console.log(left_str);
     return left_str.length;
 }
