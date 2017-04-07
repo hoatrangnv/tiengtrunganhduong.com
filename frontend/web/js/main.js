70fb67f15d872b839925fbd3bae2b72017b7aa3a
quyettvq <tvq16091993tvq>
2017-04-07 22:09:22 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -163,19 +163,19 @@ function getCaret(el) {
 
 function setCaret(elem, caretPos) {
     if(elem != null) {
-        if(elem.createTextRange) {
-            var range = elem.createTextRange();
-            range.move('character', caretPos);
-            range.select();
-        }
-        else {
+        // if(elem.createTextRange) {
+        //     var range = elem.createTextRange();
+        //     range.move('character', caretPos);
+        //     range.select();
+        // }
+        // else {
             if(elem.selectionStart) {
                 elem.focus();
                 elem.setSelectionRange(caretPos, caretPos);
             }
             else
                 elem.focus();
-        }
+        // }
     }
 }
 
