111ae5fb811f6d973634122ec0cb24a6274cc05b
quyettvq <quyettvq@gmail.com>
2017-04-07 13:55:01 +0700

    article

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -83,7 +83,7 @@
                         }
                     } while (char && (char != "\n"));
 
-                    if (last_tag.toLowerCase() === "br" || last_tag.search("/\/|--/") > -1) {
+                    if (last_tag.toLowerCase() === "br" || last_tag.search("/(\/|--)/") > -1) {
                         last_type = "";
                     }
                     if (last_type === "{" || last_type === "<>") {
