a3d2fdf867463465bcfa0906478fd875f0efe277
quyettvq <quyettvq@gmail.com>
2017-04-07 11:45:20 +0700

    article

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -75,7 +75,7 @@
                                 last_type = "<>";
                             }
                             if (last_type === ">"
-                                && (![0,1,2,3,4,5,6,7,8,9].indexOf(char) > -1 && char.toLowerCase() == char.toUpperCase())) {
+                                && (!["0","1","2","3","4","5","6","7","8","9"].indexOf(char) > -1 && char.toLowerCase() == char.toUpperCase())) {
                                 last_type = "";
                             }
                             if (last_type === ">" && last_type != char) {
