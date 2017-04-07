f551257544796422bd5d04ace629ea9e763aae69
quyettvq <quyettvq@gmail.com>
2017-04-07 17:58:36 +0700

    article

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -9,12 +9,12 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php
     $html = $model->getContentWithTemplates();
 
-    $patt = "/<code>((?:(?!<code>)[\s\S])*)<\/code>/gi";
+    $patt = "/<code>((?:(?!<code>)[\s\S])*)<\/code>/i";
     preg_match_all($patt, $html, $matches);
     foreach ($matches[1] as $inner_code) {
-        var_dump($inner_code);
-        echo "<br>";
-        var_dump(htmlspecialchars($inner_code));
+//        var_dump($inner_code);
+//        echo "<br>";
+//        var_dump(htmlspecialchars($inner_code));
         $html = str_replace($inner_code, htmlspecialchars($inner_code), $html);
     }
     ?>
