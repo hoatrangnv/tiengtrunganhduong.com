9bb1c7c4963c071e1d4d12c246eb08411132024e
quyettvq <quyettvq@gmail.com>
2017-04-07 17:43:11 +0700

    article

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -12,6 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
     $dom->loadHTML($html);
     $codes = $dom->getElementsByTagName('code');
     foreach ($codes as $code) {
+        var_dump(htmlspecialchars($code->textContent));
         $html = str_replace($code->textContent, htmlspecialchars($code->textContent), $html);
     }
 
