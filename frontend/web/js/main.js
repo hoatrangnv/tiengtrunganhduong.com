cda76f49eff673d2110c524e885168796625106e
quyettvq <tvq16091993tvq>
2017-04-08 11:22:57 +0700

    site index

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -53,13 +53,13 @@
         code_example.onfocus = function () {
             var doc = document.documentElement;
             var scroll_top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-            // setTimeout(function () {
+            setTimeout(function () {
                 var caret = getCaretOffset(code_example);
                 if (!editor_display) {
                     editor_display = true;
                     code_block.replaceChild(editor, code_example);
                 }
-            // }, 5000);
+            }, 100);
             setTimeout(function () {
                 console.log("caret " + caret);
                 setCaret(editor, caret);
