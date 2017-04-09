a746d7b7a0fc90defb6e0d6d9869feb0c697a525
quyettvq <tvq16091993tvq>
2017-04-09 23:58:33 +0700

    code example

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'My Yii Application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="<?= Url::to(['article/create']) ?>">New Article</a></p>
+        <p><a class="btn btn-lg btn-success" href="<?= Url::to(['article/create', 'code_editor' => true]) ?>">New Article</a></p>
 
         <p>
             <?php
