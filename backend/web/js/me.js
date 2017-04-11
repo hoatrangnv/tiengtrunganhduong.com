0f1604fe0404342e0ebe5f6845412ef96f97b910
quyettvq <quyettvq@gmail.com>
2017-04-11 14:50:48 +0700

    article

--- backend/web/js/me.js
+++ backend/web/js/me.js
@@ -15,6 +15,8 @@ function copyTextToClipboard (text) {
     textArea.style.position = "fixed";
     textArea.style.top = "0px";
     textArea.style.left = "0px";
+    textArea.style.width = "100px";
+    textArea.style.height = "100px";
     document.body.appendChild(textArea);
     textArea.value = text;
     textArea.select();
