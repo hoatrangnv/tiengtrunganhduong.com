b702793bdf5da934fa28fbd8d066e02f66aff392
quyettvq <quyettvq@gmail.com>
2017-04-11 10:52:14 +0700

    article

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,10 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/myStyle.css',
+        'css/me.css',
     ];
     public $js = [
-        'js/myJs.js',
+        'js/me.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
