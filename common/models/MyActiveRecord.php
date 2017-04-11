2668a1d2b7c4810f08bf60bdaab780b91fa917f2
quyettvq <tvq16091993tvq>
2017-04-11 21:58:32 +0700

    code example

--- common/models/MyActiveRecord.php
+++ common/models/MyActiveRecord.php
@@ -94,7 +94,7 @@ abstract class MyActiveRecord extends ActiveRecord implements iMyActiveRecord
         if (!$this->hasAttribute($attribute)) {
             return '';
         }
-        return Image::textWithTemplates2Html($this->$attribute);
+//        return Image::textWithTemplates2Html($this->$attribute);
     }
 
     public static function castToArray($input)
@@ -126,7 +126,7 @@ abstract class MyActiveRecord extends ActiveRecord implements iMyActiveRecord
             $query->multiple = true;
             return $query;
         }
-        throw new MethodNotFoundException('Method getChildren not found.', self::className());
+        throw new MethodNotFoundException('Method getChildren not found.', self::className(), null);
     }
 
     public static function listAsId2Name()
