27c1b938cd9207504a99488b2b17c9592c166d2a
quyettvq <tvq16091993tvq>
2017-04-15 00:08:07 +0700

    code example

--- common/models/Article.php
+++ common/models/Article.php
@@ -80,11 +80,13 @@ class Article extends \common\models\MyActiveRecord
 
     public $publish_time_timestamp;
 
+    const TIMESTAMP_FORMAT = 'Y-m-d H:i:s';
+
     public function __construct(array $config = [])
     {
         // Init publish time for new record
         if ($this->isNewRecord) {
-            $this->publish_time_timestamp = date('Y-m-d H:i:s', $this->getDefaultPublishTime());
+            $this->publish_time_timestamp = date(self::TIMESTAMP_FORMAT, $this->getDefaultPublishTime());
         }
         parent::__construct($config);
     }
@@ -92,7 +94,7 @@ class Article extends \common\models\MyActiveRecord
     public function afterFind()
     {
         // Init publish time for record found
-        $this->publish_time_timestamp = date('Y-m-d H:i:s', $this->publish_time);
+        $this->publish_time_timestamp = date(self::TIMESTAMP_FORMAT, $this->publish_time);
         parent::afterFind();
     }
 
@@ -100,6 +102,7 @@ class Article extends \common\models\MyActiveRecord
     {
         if (!$this->publish_time_timestamp) {
             $this->publish_time = $this->getDefaultPublishTime();
+            $this->publish_time_timestamp = date(self::TIMESTAMP_FORMAT, $this->publish_time);
         } else {
             $this->publish_time = strtotime($this->publish_time_timestamp);
         }
@@ -137,7 +140,7 @@ class Article extends \common\models\MyActiveRecord
             [['image_id'], 'exist', 'skipOnError' => true, 'targetClass' => Image::className(), 'targetAttribute' => ['image_id' => 'id']],
 //            [['creator_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['creator_id' => 'id']],
 //            [['updater_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updater_id' => 'id']],
-            ['publish_time_timestamp', 'date', 'format' => 'php:Y-m-d H:i:s'],
+            ['publish_time_timestamp', 'date', 'format' => 'php:' . self::TIMESTAMP_FORMAT],
         ];
     }
 
