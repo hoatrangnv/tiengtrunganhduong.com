e2194c6c31af4d5424e598342d3ea1d6417285f5
quyettvq <quyettvq@gmail.com>
2017-04-15 15:41:12 +0700

    article

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -323,9 +323,9 @@ function insertAtCaret(txtarea, text) {
 
 function textAreaAdjust(textArea) {
     var mirror = document.createElement("DIV");
-    mirror.style.cssText = "position:absolute;pointer-events:none;color:transparent;background:rgba(255,240,240,0.3)";
+    mirror.style.cssText = "position:absolute;pointer-events:none;color:transparent;background:rgba(255,100,100,0.3)";
     textArea.parentNode.insertBefore(mirror, textArea);
-    textArea.style.background = "rgba(240,255,240,0.3)";
+    textArea.style.background = "rgba(100,255,100,0.3)";
     function handleKeyEvent() {
         var text = textArea.value.replace(/</gi, "&lt;").replace(/>/gi, "&gt;") + "&nbsp;";
         mirror.innerHTML = highlightCode(text);
