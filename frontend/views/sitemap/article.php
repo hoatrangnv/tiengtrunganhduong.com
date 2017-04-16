075f0d310437e679820aadf4ea74749250b98fa3
quyettvq <tvq16091993tvq>
2017-04-16 10:09:04 +0700

    sitemap

--- frontend/views/sitemap/article.php
+++ frontend/views/sitemap/article.php
@@ -6,7 +6,7 @@
  * Time: 9:06 AM
  */
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
--- frontend/views/sitemap/index.php
+++ frontend/views/sitemap/index.php
@@ -6,7 +6,7 @@
  * Time: 8:33 AM
  */
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
 foreach ($sitemaps as $sitemap) {
