9d746785c876ff8a3d01cb1037c07d1745956623
quyettvq <quyettvq@gmail.com>
2017-04-18 09:48:22 +0700

    article

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -333,68 +333,68 @@ function textAreaAdjust(textArea) {
         var textAreaStyle = window.getComputedStyle(textArea, null);
         mirror.style.zIndex = 1 + (parseInt(textAreaStyle.getPropertyValue("z-index")) || 0);
 
-        // [
-        //     "width",
-        //     "border-box",
-        //     "-webkit-border-box",
-        //     "display",
-        //
-        //     "border-style",
-        //     "border-width",
-        //     "border-left-width",
-        //     "border-top-width",
-        //     "border-bottom-width",
-        //     "border-right-width",
-        //
-        //     "font",
-        //     "font-size",
-        //     "font-family",
-        //     "font-weight",
-        //     "font-stretch",
-        //     "line-height",
-        //     "word-spacing",
-        //     "letter-spacing",
-        //     "tab-size",
-        //     "-moz-tab-size",
-        //     "text-indent",
-        //     "text-transform",
-        //
-        //     "padding",
-        //     "padding-left",
-        //     "padding-top",
-        //     "padding-bottom",
-        //     "padding-right",
-        //
-        //     "word-wrap",
-        //     "white-space",
-        //     "word-break",
-        //
-        //     "overflow",
-        //     "overflow-x",
-        //     "overflow-y"
-        // ].forEach(
-        var myCSS = [];
-        for (var i = 0; i < textAreaStyle.length; i++) {
-            myCSS.push(textAreaStyle[i]);
-        }
-        myCSS.forEach(
+        [
+            "width",
+            "border-box",
+            "-webkit-border-box",
+            "display",
+
+            "border-style",
+            "border-width",
+            "border-left-width",
+            "border-top-width",
+            "border-bottom-width",
+            "border-right-width",
+
+            "font",
+            "font-size",
+            "font-family",
+            "font-weight",
+            "font-stretch",
+            "line-height",
+            "word-spacing",
+            "letter-spacing",
+            "tab-size",
+            "-moz-tab-size",
+            "text-indent",
+            "text-transform",
+
+            "padding",
+            "padding-left",
+            "padding-top",
+            "padding-bottom",
+            "padding-right",
+
+            "word-wrap",
+            "white-space",
+            "word-break",
+
+            "overflow",
+            "overflow-x",
+            "overflow-y"
+        ].forEach(
+        // var myCSS = [];
+        // for (var i = 0; i < textAreaStyle.length; i++) {
+        //     myCSS.push(textAreaStyle[i]);
+        // }
+        // myCSS.forEach(
             function (prop) {
-                if ([
-                        "pointer-events",
-                        "position",
-                        "z-index",
-                        "height",
-                        "left",
-                        "top",
-                        "right",
-                        "bottom",
-                        "color",
-                        "background"
-                    ].indexOf(prop) === -1) {
+                // if ([
+                //         "pointer-events",
+                //         "position",
+                //         "z-index",
+                //         "height",
+                //         "left",
+                //         "top",
+                //         "right",
+                //         "bottom",
+                //         "color",
+                //         "background"
+                //     ].indexOf(prop) === -1) {
                         if (typeof mirror.style[prop] !== "undefined") {
                             mirror.style[prop] = textAreaStyle.getPropertyValue(prop);
                         }
-                }
+                // }
             }
         );
         textArea.style.height = window.getComputedStyle(mirror, null).getPropertyValue("height");
