00ecc8d0052dce3e2da463122d24d253a820f77c
quyettvq <quyettvq@gmail.com>
2017-04-25 18:48:11 +0700

    code editor

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -284,15 +284,15 @@ function textAreaAdjust(textArea) {
     // Handle events
     textArea.addEventListener("keydown", handleKeyEvent);
     textArea.addEventListener("keyup", handleKeyEvent);
-    // textArea.addEventListener("scroll", setMirrorScroll);
+    textArea.addEventListener("scroll", setMirrorScroll);
     textArea.addEventListener("resize", function () {
         setMirrorStyle();
         setMirrorScroll();
     });
-    textArea.addEventListener("scroll", function () {
-        setMirrorStyle();
-        setMirrorScroll();
-    });
+    // textArea.addEventListener("scroll", function () {
+    //     setMirrorStyle();
+    //     setMirrorScroll();
+    // });
 
 
     // Functions
