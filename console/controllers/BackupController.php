31c500fff51c1657c26e1b0d002f864b0a7bd485
vanquyettran <tvq16091993tvq>
2017-04-30 18:20:16 +0700

    backup

--- console/controllers/BackupController.php
+++ console/controllers/BackupController.php
@@ -7,9 +7,25 @@ use yii\console\Controller;
 class BackupController extends Controller
 {
     public $mysql_pwd;
+    public function options($actionID)
+    {
+        $result = [];
+        switch ($actionID) {
+            case 'db':
+                $result = ['mysql_pwd'];
+                break;
+            default:
+                break;
+        }
+        return $result;
+    }
+    public function optionAliases()
+    {
+        return ['p' => 'mysql_pwd'];
+    }
     public function actionDb()
     {
-        $filename = \Yii::getAlias('@backups/' . date('Ymd') . '/db.sql');
+        $filename = \Yii::getAlias('@backups/' . date('Ymd') . '/' . date('His') . '_db.sql');
         exec(
             "mysqldump -P 3306 -h 127.6.245.2 -u adminRxtAczm -p quyettran_com --password=\"{$this->mysql_pwd}\" >db/$filename",
             $output
@@ -17,6 +33,22 @@ class BackupController extends Controller
         if ($output == '') {
             /* no output is good */
             echo $filename;
+        } else {
+            /* we have something to log the output here*/
+            echo $output;
+        }
+    }
+    public function actionImages()
+    {
+        $destination = \Yii::getAlias('@backups/' . date('Ymd') . '/' . date('His') . '_images.sql');
+        $source = \Yii::getAlias('@images');
+        exec(
+            "tar -zcvf \"{$destination}\" \"{$source}\"",
+            $output
+        );
+        if ($output == '') {
+            /* no output is good */
+            echo $destination;
         } else {
             /* we have something to log the output here*/
             echo $output;
