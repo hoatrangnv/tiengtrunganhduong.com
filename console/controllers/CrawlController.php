90ef39f949154d4f8cd5dd3f429d9e8d7d14fdfc
vanquyettran <tvq16091993tvq>
2017-06-04 01:21:53 +0700

    crawl

--- console/controllers/CrawlController.php
+++ console/controllers/CrawlController.php
@@ -72,7 +72,7 @@ class CrawlController extends Controller
         ini_set('memory_limit', '1024M');
         $i = 0;
         $k = 0;
-        $data = CrawledPage::find()->limit(100)->all();
+        $data = CrawledPage::find()->all();
         foreach ($data as $key => $item) {
             echo "$key\n";
         }
