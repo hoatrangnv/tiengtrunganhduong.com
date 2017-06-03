1760cd67476872cded2e09dbf20f06b3dc60d8e1
vanquyettran <tvq16091993tvq>
2017-06-04 01:21:23 +0700

    crawl

--- console/controllers/CrawlController.php
+++ console/controllers/CrawlController.php
@@ -73,6 +73,10 @@ class CrawlController extends Controller
         $i = 0;
         $k = 0;
         $data = CrawledPage::find()->limit(100)->all();
+        foreach ($data as $key => $item) {
+            echo "$key\n";
+        }
+        return;
         foreach ($data as $item) {
             $k++;
             if ($i > 9) {
