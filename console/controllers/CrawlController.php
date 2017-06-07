960d59f9bea6a1a7b3e4fe8556e51340bc67fa28
vanquyettran <tvq16091993tvq>
2017-06-07 23:12:02 +0700

    crawl

--- console/controllers/CrawlController.php
+++ console/controllers/CrawlController.php
@@ -114,7 +114,7 @@ class CrawlController extends Controller
             echo 2 . "\n";
 //            var_dump($k, $h1, $content);
             if ($h1 && $content && substr($relative_url, -4) === '.htm' && strpos($relative_url, '/') === false) {
-                if ($article = Article::find()->where(['slug' => str_replace('.htm', '', $relative_url)])->one()) {
+                if ($article = Article::find()->where(['slug' => $relative_url])->orWhere(['slug' => str_replace('.htm', '', $relative_url)])->one()) {
 //                    if ($time_div = $dom->find('div.timeNewTop', 0)) {
 //                        $view_count = (int) str_replace(['lượt xem', '-', ' '], '', substr($time_div->innerHTML, 11));
 //                        $article->view_count = $view_count;
@@ -143,6 +143,9 @@ class CrawlController extends Controller
 //                    }
                     echo "article id = $article->id\n";
                     if ($article->id < 21) {
+                        if (strpos($article->slug, '.htm') !== false) {
+                            $article->slug = str_replace('.htm', '', $relative_url);
+                        }
                         $article->content = $content->innerHTML;
                         if ($article->save()) {
                             echo $article->id . "\n";
