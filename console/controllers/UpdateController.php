2fe93009d5f4684659d4a83120da61c873f51109
vanquyettran <tvq16091993tvq>
2017-06-09 20:54:18 +0700

    article category

---
+++ console/controllers/UpdateController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 6/9/2017
+ * Time: 8:50 PM
+ */
+
+namespace console\controllers;
+
+
+use backend\models\ArticleCategory;
+use yii\console\Controller;
+
+class UpdateController extends Controller
+{
+    public function actionArticleCategories()
+    {
+        foreach (ArticleCategory::find()->all() as $category) {
+            if ($category->type == ArticleCategory::TYPE_SERVICE) {
+                $category->parent_id = 41;
+                echo $category->name . "\n";
+            }
+        }
+    }
+}
