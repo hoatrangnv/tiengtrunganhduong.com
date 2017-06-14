d57fa74da7eba8a96def67cf3782be560e836576
vanquyettran <tvq16091993tvq>
2017-06-15 03:24:20 +0700

    site

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -83,6 +83,8 @@ class BaseController extends Controller
         $this->menu = new Menu();
         $this->menu->init(['_' => $data0, '__' => $data1]);
 
+        var_dump($this->menu->getRootItems());
+
         return parent::beforeAction($action);
     }
 }
