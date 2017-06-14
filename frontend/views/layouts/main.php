d163ad6adaabed405b39e7cf339f1d16d20aaf83
vanquyettran <tvq16091993tvq>
2017-06-15 02:30:22 +0700

    site

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -71,10 +71,10 @@ $this->registerMetaTag([
             <div>Hotline: <?= ($item = SiteParam::findOneByName(SiteParam::PHONE_NUMBER)) ? $item->value : '' ?></div>
         </div>
         <div class="social-networks">
-            <a title="facebook" href="<?= ($item = SiteParam::findOneByName(SiteParam::FACEBOOK_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank"><i class="icon facebook-icon"></i></a>
-            <a title="twitter" href="<?= ($item = SiteParam::findOneByName(SiteParam::TWITTER_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank"><i class="icon twitter-icon" ></i></a>
-            <a title="google plus" href="<?= ($item = SiteParam::findOneByName(SiteParam::GOOGLE_PLUS_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank"><i class="icon google-plus-icon"></i></a>
-            <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank"><i class="icon youtube-icon"></i></a>
+            <a title="facebook" href="<?= ($item = SiteParam::findOneByName(SiteParam::FACEBOOK_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon facebook-icon"></i></a>
+            <a title="twitter" href="<?= ($item = SiteParam::findOneByName(SiteParam::TWITTER_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon twitter-icon" ></i></a>
+            <a title="google plus" href="<?= ($item = SiteParam::findOneByName(SiteParam::GOOGLE_PLUS_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon google-plus-icon"></i></a>
+            <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon youtube-icon"></i></a>
         </div>
     </div>
 </footer>
