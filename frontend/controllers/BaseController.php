9fe2231c34bd2fb5fe644ff897ee4fa1019ad44d
vanquyettran <tvq16091993tvq>
2017-06-15 03:48:30 +0700

    site

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -80,6 +80,7 @@ class BaseController extends Controller
             }
         }
 
+
         $this->menu = new Menu();
         $this->menu->init(['_' => $data0, '__' => $data1]);
 
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -66,6 +66,8 @@ $this->registerMetaTag([
     <div class="container clr">
         <div>
             <h3><?= ($item = SiteParam::findOneByName(SiteParam::COMPANY_NAME)) ? $item->value : '' ?></h3>
+        </div>
+        <div>
             <div><?= Yii::t('app', 'Address') ?>: <?= ($item = SiteParam::findOneByName(SiteParam::ADDRESS)) ? $item->value : '' ?></div>
             <div>Email: <?= ($item = SiteParam::findOneByName(SiteParam::EMAIL)) ? $item->value : '' ?></div>
             <div>Hotline: <?= ($item = SiteParam::findOneByName(SiteParam::PHONE_NUMBER)) ? $item->value : '' ?></div>
