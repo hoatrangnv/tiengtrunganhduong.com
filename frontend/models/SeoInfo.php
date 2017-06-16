ab8b8f7a521b9976348d3e284d545416f206fd2d
vanquyettran <tvq16091993tvq>
2017-06-17 00:28:30 +0700

    site

---
+++ frontend/models/SeoInfo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 6/17/2017
+ * Time: 12:22 AM
+ */
+
+namespace frontend\models;
+
+
+class SeoInfo extends \common\models\SeoInfo
+{
+    public static function findModel()
+    {
+
+    }
+}
