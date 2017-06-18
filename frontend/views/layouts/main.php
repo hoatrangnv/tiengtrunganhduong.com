fadbf3cfe68a66ce695d66e7aa43e7eba00b22c3
vanquyettran <tvq16091993tvq>
2017-06-19 02:10:58 +0700

    chinese lantern

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -66,30 +66,7 @@ $seoInfo->registerLinkTags($this);
     </div>
 </div>
 
-<footer class="wrap">
-    <div class="container clr">
-        <div>
-            <h3 class="company-name"><?= ($item = SiteParam::findOneByName(SiteParam::COMPANY_NAME)) ? $item->value : '' ?></h3>
-        </div>
-        <div>
-            <div><?= Yii::t('app', 'Address') ?>: <?= ($item = SiteParam::findOneByName(SiteParam::ADDRESS)) ? $item->value : '' ?></div>
-            <div>Email: <?= ($item = SiteParam::findOneByName(SiteParam::EMAIL)) ? $item->value : '' ?></div>
-            <div>Hotline: <?= ($item = SiteParam::findOneByName(SiteParam::PHONE_NUMBER)) ? $item->value : '' ?></div>
-        </div>
-        <div class="social-networks">
-            <a title="facebook" href="<?= ($item = SiteParam::findOneByName(SiteParam::FACEBOOK_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon facebook-icon"></i></a>
-            <a title="twitter" href="<?= ($item = SiteParam::findOneByName(SiteParam::TWITTER_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon twitter-icon" ></i></a>
-            <a title="google plus" href="<?= ($item = SiteParam::findOneByName(SiteParam::GOOGLE_PLUS_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon google-plus-icon"></i></a>
-            <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon youtube-icon"></i></a>
-        </div>
-        <div>
-            <a href="http://www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" title="DMCA.com Protection Status" class="dmca-badge">
-                <img src="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" alt="DMCA.com Protection Status">
-            </a>
-            <script src="//images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
-        </div>
-    </div>
-</footer>
+<?php require_once 'footer.php' ?>
 
 <!--<script><?php /*require_once Yii::getAlias('@webroot/js/main.js') */?></script>-->
 <?php require_once 'fbMessenger.php' ?>
