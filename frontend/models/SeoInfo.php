91774a6e5b21d06420bc44baddb6cf63cfd58c65
vanquyettran <tvq16091993tvq>
2017-06-18 18:46:31 +0700

    viewport

--- frontend/models/SeoInfo.php
+++ frontend/models/SeoInfo.php
@@ -121,7 +121,7 @@ class SeoInfo extends \common\models\SeoInfo
         ]);
         $view->registerMetaTag([
             'name' => 'viewport',
-            'content' => 'width=device-width, initial-scale=1'
+            'content' => 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, shrink-to-fit=no, user-scalable=no'
         ]);
     }
 
