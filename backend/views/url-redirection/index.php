35f7a92a4192d828cb1063c6fc67ab1a61cc4e53
vanquyettran <vanquyettran@hotmail.com>
2017-06-20 12:09:14 +0700

    GA

--- backend/views/url-redirection/index.php
+++ backend/views/url-redirection/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+//            'id',
             'from_url',
             'to_url',
             'active:boolean',
