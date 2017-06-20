0e44ccd8debb1a1e3e05455c6c7f8d5a0946d2d4
vanquyettran <vanquyettran@hotmail.com>
2017-06-20 09:13:28 +0700

    GA

--- frontend/models/UrlRedirection.php
+++ frontend/models/UrlRedirection.php
@@ -76,6 +76,8 @@ class UrlRedirection extends \common\models\UrlRedirection
             }
         }
 
+        var_dump($from_url, $to_url);die;
+
         if ($to_url) {
             return Yii::$app->response->redirect($to_url, 301);
         } else {
