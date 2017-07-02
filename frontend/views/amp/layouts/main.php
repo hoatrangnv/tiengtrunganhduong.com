d086b87ffcc257455506487cac7d8f28248a4a89
vanquyettran <tvq16091993tvq>
2017-07-02 22:52:32 +0700

    amp

--- frontend/views/amp/layouts/main.php
+++ frontend/views/amp/layouts/main.php
@@ -57,7 +57,7 @@ $seoInfo->registerLinkTags($this);
             "datePublished": "<?= date('Y-m-dTH:i:sZ', $seoInfo->create_time) ?>",
             "dateModified": "<?= date('Y-m-dTH:i:sZ', $seoInfo->update_time) ?>",
             "image": [
-                "<?= ($image_src = $seoInfo->image ? $seoInfo->image->getSource() : '') ? $image_src : Url::home(true) . 'img/logo_banner.png' ?>"
+                "<?= ($image_src = $seoInfo->image ? $seoInfo->image->getSource() : '') ? $image_src : Url::home(true) . 'img/default.png' ?>"
             ]
         }
     </script>
