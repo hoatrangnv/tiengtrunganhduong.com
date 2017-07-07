2ef068c9e09dd3197b2a12220c799064dee9a238
vanquyettran <vanquyettran@hotmail.com>
2017-07-07 16:54:23 +0700

    module

--- common/modules/image/views/default/_form.php
+++ common/modules/image/views/default/_form.php
@@ -72,7 +72,7 @@ if ($model->isNewRecord) {
                     function addInputResizeKey() {
                         var input = document.getElementById("input-resize-key");
                         var select = document.getElementById("<?= Html::getInputId($model, 'input_resize_keys') ?>");
-                        var matches = input.value.match(/(\D*)(\d+)x(\d+)(\D*)/g);
+                        var matches = input.value.match(/(\D*)(\d+)x(\d+)(\D*)/);
                         var value;
                         if (matches && matches[0] && matches[0][2] && matches[0][3]) {
                             value = matches[0][2] + "x" + matches[0][3];
