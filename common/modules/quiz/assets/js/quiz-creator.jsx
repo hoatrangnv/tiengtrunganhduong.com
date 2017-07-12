b27c2b0b6af72aebf6bf53c941591925ecd300a7
vanquyettran <tvq16091993tvq>
2017-07-13 01:09:38 +0700

    quiz

--- common/modules/quiz/assets/js/quiz-creator.jsx
+++ common/modules/quiz/assets/js/quiz-creator.jsx
@@ -2,130 +2,311 @@
  * Created by User on 7/11/2017.
  */
 
-class TodoApp extends React.Component {
+class Quiz extends React.Component {
     constructor(props) {
         super(props);
-        this.addItem = this.addItem.bind(this);
-        this.state = {items: [
-            {
-                text: "Quiz Info",
-                id: Date.now(),
-                order: 1,
-                active: true,
-                form: {
-                    name: "Quiz Info",
-                    fields: [
+        this.state = {
+            items: [
+                {
+                    id: Math.random(),
+                    name: "Meta",
+                    active: true,
+                    inputs: [
                         {
+                            id: Math.random(),
+                            type: "text",
                             name: "name",
-                            type: "text"
+                            label: "Quiz Name",
+                            validate: function (value) {
+                                var isOkay = "string" === typeof value && "" !== value.trim();
+                                var errorMsg = "";
+                                if (!isOkay) {
+                                    errorMsg = "Quiz Name cannot be empty";
+                                }
+                                return errorMsg;
+                            }
                         },
                         {
+                            id: Math.random(),
+                            type: "textArea",
                             name: "description",
-                            type: "text"
+                            label: "Description About Quiz",
+                            validate: function (value) {
+                                var isOkay = "string" === typeof value && "" !== value.trim();
+                                var errorMsg = "";
+                                if (!isOkay) {
+                                    errorMsg = "Quiz Description cannot be empty";
+                                }
+                                return errorMsg;
+                            }
+                        }
+                    ]
+                },
+                {
+                    id: Math.random(),
+                    active: false,
+                    name: "Results",
+                    inputs: [
+                        {
+                            id: Math.random(),
+                            type: "selectBox",
+                            name: "canvasSize",
+                            label: "Canvas Size",
+                            options: [
+                                {id: Math.random(), value: "720x377", text: "720x377"},
+                                {id: Math.random(), value: "360x292", text: "360x292"}
+                            ],
+                            validate: function (value) {
+                                return "";
+                            }
                         }
                     ]
                 }
-            }
-        ], text: ""};
+            ]
+        };
     }
 
     render() {
-        this.state.items.sort(function (a, b) {
-            return a.order - b.order;
-        });
-        console.log(this.state.items);
+        var activeItem = this.state.items.find((item) => (item.active));
+        var formInputs = [];
+        if (activeItem) {
+            formInputs = activeItem.inputs;
+        }
         return (
             <div>
-                <h3>TODO</h3>
-                <TodoList items={this.state.items} addItem={this.addItem} />
-                <Form fields={this.state.items.find(function (item) {
-                    return item.active;
-                }).form.fields} />
+                <TabBar items={this.state.items} />
+                <Form inputs={formInputs} />
             </div>
         );
     }
+}
 
-    addItem() {
-        this.state.items.forEach(function (item) {
+class TabBar extends React.Component {
+    render() {
+        return (
+            <ul className="tab-list">
+                {
+                    this.props.items.map((item, index, items) => (
+                        <Tab key={item.id} index={index} items={items} />
+                    ))
+                }
+            </ul>
+        );
+    }
+}
+
+class Tab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        this.props.items.forEach((item) => {
             item.active = false;
         });
-        var newItem = {
-            text: "Step " + (this.state.items.length + 1),
-            id: Date.now(),
-            order: this.state.items.length + 1,
-            active: true,
-            form: {
-                name: "",
-                fields: []
-            }
-        };
-        this.setState((prevState) => ({
-            items: prevState.items.concat(newItem),
-            text: ""
-        }));
+        this.props.items[this.props.index].active = true;
+        DOMRender();
+    }
+
+    render() {
+        var item = this.props.items[this.props.index];
+        return (
+            <li className={item.active ? "active" : ""} onClick={this.handleClick}>{item.name}</li>
+        );
     }
 }
 
 class Form extends React.Component {
     render() {
         return (
-            <form>{
-                this.props.fields.map((item, index, items) => {
-                    return (<input name={item.name} type={item.type} />);
-                })
-            }</form>
+            <form>
+                {
+                    this.props.inputs.map((item) => (
+                        <FormInput key={item.id} type={item.type} name={item.name} label={item.label} options={item.options} validate={item.validate} />
+                    ))
+                }
+            </form>
         );
     }
 }
 
-class TodoList extends React.Component {
+class FormInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            value: "",
+            errorMsg: ""
+        };
+    }
+
+    handleChange(event) {
+        this.state.value = event.target.value;
+        this.state.errorMsg = this.props.validate(this.state.value);
+        if ("" !== this.state.errorMsg) {
+            DOMRender();
+        }
+    }
+
     render() {
+        var type = this.props.type;
+        var name = this.props.name;
+        var input;
+        switch (type) {
+            case "textArea":
+                input = <textArea name={name} onChange={this.handleChange} onBlur={this.handleChange} />;
+                break;
+            case "selectBox":
+                input = <select name={name} onChange={this.handleChange} onBlur={this.handleChange}>
+                    {
+                        this.props.options.map((option) => (
+                            <option key={option.id} value={option.value}>{option.text}</option>
+                        ))
+                    }
+                </select>;
+                break;
+            default:
+                input = <input type={type} name={name} onChange={this.handleChange} onBlur={this.handleChange} />;
+        }
         return (
-            <ul className="tab-list">
-                {this.props.items.map((item, index, items) => (
-                    <li
-                        key={item.id}
-                        className={item.active ? "active" : ""}
-                        onClick={(e) => {
-                            if (e.target.className != "move-button") {
-                                items.forEach(function (item) {
-                                    item.active = false;
-                                });
-                                item.active = true;
-                                DOMRender();
-                            }
-                        }}
-                    >
-                        <button className="move-button left-move" onClick={() => {
-                            if (items[index - 1]) {
-                                item.order --;
-                                items[index - 1].order ++;
-                                DOMRender();
-                            }
-                        }}>&lt;</button>
-                        <span className="text">{item.text}</span>
-                        <button className="move-button right-move" onClick={() => {
-                            if (items[index + 1]) {
-                                item.order ++;
-                                items[index + 1].order --;
-                                DOMRender();
-                            }
-                        }}>&gt;</button>
-                    </li>
-                ))}
-                <li
-                    onClick={() => {
-                        this.props.addItem();
-                        DOMRender();
-                    }}
-                ><i>add#{this.props.items.length + 1}</i></li>
-            </ul>
+            <div>
+                <label>{this.props.label}</label>
+                <div>{input}</div>
+                <div>{this.state.errorMsg}</div>
+            </div>
         );
     }
 }
 
 DOMRender();
 function DOMRender() {
-    ReactDOM.render(<TodoApp />, document.getElementById("root"));
+    ReactDOM.render(<Quiz />, document.getElementById("root"));
 }
 
+// class TodoApp extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.addItem = this.addItem.bind(this);
+//         this.state = {items: [
+//             {
+//                 text: "Quiz Info",
+//                 id: Date.now(),
+//                 order: 1,
+//                 active: true,
+//                 form: {
+//                     name: "Quiz Info",
+//                     fields: [
+//                         {
+//                             name: "name",
+//                             type: "text"
+//                         },
+//                         {
+//                             name: "description",
+//                             type: "text"
+//                         }
+//                     ]
+//                 }
+//             }
+//         ]};
+//     }
+//
+//     render() {
+//         this.state.items.sort(function (a, b) {
+//             return a.order - b.order;
+//         });
+//         console.log(this.state.items);
+//         return (
+//             <div>
+//                 <h3>TODO</h3>
+//                 <TodoList items={this.state.items} addItem={this.addItem} />
+//                 <Form fields={this.state.items.find(function (item) {
+//                     return item.active;
+//                 }).form.fields} />
+//             </div>
+//         );
+//     }
+//
+//     addItem() {
+//         this.state.items.forEach(function (item) {
+//             item.active = false;
+//         });
+//         var newItem = {
+//             text: "Step " + (this.state.items.length + 1),
+//             id: Date.now(),
+//             order: this.state.items.length + 1,
+//             active: true,
+//             form: {
+//                 name: "",
+//                 fields: []
+//             }
+//         };
+//         this.setState((prevState) => ({
+//             items: prevState.items.concat(newItem)
+//         }));
+//     }
+// }
+//
+// class Form extends React.Component {
+//     render() {
+//         return (
+//             <form>{
+//                 this.props.fields.map((item, index, items) => {
+//                     return (<input name={item.name} type={item.type} />);
+//                 })
+//             }</form>
+//         );
+//     }
+// }
+//
+// class TodoList extends React.Component {
+//     render() {
+//         return (
+//             <ul className="tab-list">
+//                 {this.props.items.map((item, index, items) => (
+//                     <li
+//                         key={item.id}
+//                         className={item.active ? "active" : ""}
+//                         onClick={(e) => {
+//                             if (e.target.className != "move-button") {
+//                                 items.forEach(function (item) {
+//                                     item.active = false;
+//                                 });
+//                                 item.active = true;
+//                                 DOMRender();
+//                             }
+//                         }}
+//                     >
+//                         <button className="move-button left-move" onClick={() => {
+//                             if (items[index - 1]) {
+//                                 item.order --;
+//                                 items[index - 1].order ++;
+//                                 DOMRender();
+//                             }
+//                         }}>&lt;</button>
+//                         <span className="text">{item.text}</span>
+//                         <button className="move-button right-move" onClick={() => {
+//                             if (items[index + 1]) {
+//                                 item.order ++;
+//                                 items[index + 1].order --;
+//                                 DOMRender();
+//                             }
+//                         }}>&gt;</button>
+//                     </li>
+//                 ))}
+//                 <li
+//                     onClick={() => {
+//                         this.props.addItem();
+//                         DOMRender();
+//                     }}
+//                 ><i>add#{this.props.items.length + 1}</i></li>
+//             </ul>
+//         );
+//     }
+// }
+//
+// DOMRender();
+// function DOMRender() {
+//     ReactDOM.render(<TodoApp />, document.getElementById("root"));
+// }
+//
