07294b876d9ff9e7ce06b619e87ef3b4b691f671
vanquyettran <vanquyettran@hotmail.com>
2017-09-05 18:32:30 +0700

    q

--- frontend/views/quiz/play.php
+++ frontend/views/quiz/play.php
@@ -88,6 +88,23 @@ $this->params['breadcrumbs'][] = $this->title;
         requestCharacterRealData: requestUserData
     };
     //==============================================
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1793387650973425',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v2.5'
+        });
+        FB.AppEvents.logPageView();
+    };
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    //==============================================
     var userID;
     var accessToken;
     function requestUserData(userType, media, callback) {
