f39204411801f949b2eeb1b51a763d74aff32b35
vanquyettran <tvq16091993tvq>
2017-11-12 22:18:24 +0700

    fix sitemap image url error

--- frontend/controllers/SitemapController.php
+++ frontend/controllers/SitemapController.php
@@ -107,7 +107,7 @@ class SitemapController extends Controller
             $image = $article->getImage()->oneActive();
             if ($image) {
                 $url['image'] = [
-                    'loc' => rawurldecode($image->getSource()),
+                    'loc' => urldecode($image->getSource()),
                     'title' => Html::encode($image->name),
                     'caption' => Html::encode($image->name)
                 ];
