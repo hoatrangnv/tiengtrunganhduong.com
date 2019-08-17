aa50275ab1bb40d3d951169b9b3f9ba9e99beccf
vanquyettran <tvq16091993tvq>
2019-08-17 16:13:24 +0700

    add schema to amp

--- frontend/views/layouts/breadcrumb.php
+++ frontend/views/layouts/breadcrumb.php
@@ -9,36 +9,39 @@ use yii\helpers\Url;
 
 $canonicalLink = $this->context->canonicalLink;
 $links = isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [];
-?>
-<ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
-    <?php
-    array_unshift($links, ['url' => Url::home(true), 'label' => Yii::t('app', 'Homepage')]);
-    $pos = 0;
-    foreach ($links as $link) {
-        $pos++;
-        $item = [];
-        if (is_string($link)) {
-            $item['url'] = $canonicalLink;
-            $item['label'] = $link;
-        } else if (isset($link['url']) && isset($link['label'])) {
-            $item = $link;
-        }
+if (!empty($links)) {
+    ?>
+    <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+        <?php
+        array_unshift($links, ['url' => Url::home(true), 'label' => Yii::t('app', 'Homepage')]);
+        $pos = 0;
+        foreach ($links as $link) {
+            $pos++;
+            $item = [];
+            if (is_string($link)) {
+                $item['url'] = $canonicalLink;
+                $item['label'] = $link;
+            } else if (isset($link['url']) && isset($link['label'])) {
+                $item = $link;
+            }
 
-        if ($pos > 1) {
+            if ($pos > 1) {
+                ?>
+                <span class="divider">/</span>
+                <?php
+            }
             ?>
-            <span class="divider">/</span>
+
+            <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                <a href="<?= $item['url'] ?>" title="<?= $item['label'] ?>" itemprop="item">
+                    <span itemprop="name"><?= $item['label'] ?></span>
+                </a>
+                <meta itemprop="position" content="<?= $pos ?>"/>
+            </li>
+
             <?php
         }
         ?>
-
-        <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-            <a href="<?= $item['url'] ?>" title="<?= $item['label'] ?>" itemprop="item">
-                <span itemprop="name"><?= $item['label'] ?></span>
-            </a>
-            <meta itemprop="position" content="<?= $pos ?>" />
-        </li>
-
-        <?php
-    }
-    ?>
-</ol>
+    </ol>
+    <?php
+}
