e702faade189cbd77f9a551b19982be9b4c4bb24
Tran Van Quyet <vanquyettran@gmail.com>
2019-09-09 14:06:31 +0700

    fix dmca err

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -28,7 +28,7 @@ use frontend\models\SiteParam;
             <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon youtube-icon"></i></a>
         </div>
         <div>
-            <a href="http://www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" title="DMCA.com Protection Status" class="dmca-badge">
+            <a href="//www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" title="DMCA.com Protection Status" class="dmca-badge">
                 <img src="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" alt="DMCA.com Protection Status">
             </a>
             <script src="//images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
