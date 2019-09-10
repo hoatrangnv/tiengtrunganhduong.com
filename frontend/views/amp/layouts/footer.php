af54cbed79d349b1288b7f38b85586ec733770ca
Tran Van Quyet <vanquyettran@gmail.com>
2019-09-10 17:28:53 +0700

    update dmca

--- frontend/views/amp/layouts/footer.php
+++ frontend/views/amp/layouts/footer.php
@@ -28,10 +28,5 @@ use frontend\models\SiteParam;
             <a title="google plus" href="<?= ($item = SiteParam::findOneByName(SiteParam::GOOGLE_PLUS_URL)) ? $item->value : '#' ?>" target="_blank" rel="nofollow"><i class="icon google-plus-icon"></i></a>
             <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : '#' ?>" target="_blank" rel="nofollow"><i class="icon youtube-icon"></i></a>
         </div>
-        <div>
-            <a href="http://www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" title="DMCA.com Protection Status" class="dmca-badge">
-                <amp-img src="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" width="121" height="24" layout="responsive" alt="DMCA.com Protection Status">
-            </a>
-        </div>
     </div>
 </footer>
