815ba060cbb51dc574c8b04768b68101d8acceae
Tran Van Quyet <vanquyettran@gmail.com>
2019-09-10 17:26:47 +0700

    update dmca

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -28,10 +28,12 @@ use frontend\models\SiteParam;
             <a title="youtube" href="<?= ($item = SiteParam::findOneByName(SiteParam::YOUTUBE_URL)) ? $item->value : 'javascript:void(0)' ?>" target="_blank" rel="nofollow"><i class="icon youtube-icon"></i></a>
         </div>
         <div>
-            <a href="//www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" title="DMCA.com Protection Status" class="dmca-badge">
-                <img src="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0" alt="DMCA.com Protection Status">
+            <a href="//www.dmca.com/Protection/Status.aspx?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0"
+               title="DMCA.com Protection Status" class="dmca-badge">
+                <img src="https://images.dmca.com/Badges/_dmca_premi_badge_4.png?ID=5a851896-94bd-4f2e-89fd-79369a67a3a0"
+                     alt="DMCA.com Protection Status"/>
             </a>
-            <script src="//images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+            <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></script>
         </div>
     </div>
 </footer>
