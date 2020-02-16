5bf736d8f85312956488e43c3f4893a51c62ce04
vanquyettran <tvq16091993tvq>
2020-02-16 14:46:11 +0700

    update phonetic lookup result render

--- frontend/views/lookup-phonetic-for-chinese-text/index.php
+++ frontend/views/lookup-phonetic-for-chinese-text/index.php
@@ -46,20 +46,20 @@ $chinese_text_analyzer_src = Yii::getAlias('@web/js/chinese_text_analyzer.js');
             "class": "search-form clr",
             onsubmit: function (event) {
                 event.preventDefault();
-                renderResult(input.value);
+                requestAndUpdateResult(input.value);
             }
         }
     );
     appendChildren(translationRoot, [form, result]);
-    renderResult(search_text);
+    requestAndUpdateResult(search_text);
 
-    function renderResult(search) {
+    function requestAndUpdateResult(search) {
         if ("string" == typeof search && search.trim()) {
             empty(result);
             result.appendChild(
                 elm("div", "Đang xử lý...", {'class': 'processing'})
             );
-            requestTranslation(
+            requestPhoneticApi(
                 search,
                 handleOnRequestSuccess,
                 handleOnRequestError
@@ -67,56 +67,114 @@ $chinese_text_analyzer_src = Yii::getAlias('@web/js/chinese_text_analyzer.js');
         }
     }
 
-    function handleOnRequestSuccess(data) {
+    function handleOnRequestSuccess(data, inputParseResult) {
         var null_replacement = '_';
         var wordsJoiner = '';
 
         var executedWordsInfo = data['executedWordsInfo'];
         var phrasesData = data['phrasesData'];
+        var inputMixedParts = inputParseResult[0];
+        var inputLetterPartIndexes = inputParseResult[1];
 
-        var viewItems = [];
+        var paragraphViewArrItems = [];
+        var detailsViewElmItems = [];
+
+        var renderDetailsView = function () {
+            appendChildren(result, [
+                elm('h2', 'Chi tiết'),
+                elm('div', detailsViewElmItems.map(function (elmArr) {
+                    return elm('div', elmArr, {'class': 'details-item'});
+                }, []), {'class': 'details-view'})
+            ]);
+        };
+        
+        var renderParagraphView = function () {
+            appendChildren(result, [elm('h2', 'Kết quả')]);
+            var paragrapViewArrItemIndex = -1;
+            appendChildren(result, elm('div', inputMixedParts.map(function (partChars, index) {
+                if (inputLetterPartIndexes.indexOf(index) > -1) {
+                    paragrapViewArrItemIndex++;
+                    return paragraphViewArrItems[paragrapViewArrItemIndex];
+                } else {
+                    return partChars.map(function (char) {
+                        return [char, char, char];
+                    });
+                }
+            }).reduce(function (arr, item, index) {
+                return arr.concat(item);
+            }, []).map(function (item) {
+                return elm('div', item.map(function (val) {
+                    return elm('div', val);
+                }));
+            }), {'class': 'paragraph-view'}));
+        };
+
+        var renderViews = function () {
+            empty(result);
+            renderParagraphView();
+            renderDetailsView();
+        };
+
         var heavyTasks = [];
         var currentHeavyTaskIndex = -1;
         executedWordsInfo.forEach(function (infoItem, index) {
-            viewItems[index] = null;
+            detailsViewElmItems[index] = null;
             var error = infoItem['error'];
             var words = infoItem['words'];
             var phraseMaxWords = infoItem['phraseMaxWords'];
             if (error) {
-                viewItems[index] = [
+                detailsViewElmItems[index] = [
                     elm('h3', words.join(wordsJoiner)),
                     elm('div', error, {'class': 'error'})
                 ];
+                paragraphViewArrItems[index] = words.map(function (word) {
+                    return [word, null_replacement, null_replacement];
+                });
             } else {
-                viewItems[index] = null;
-
+                detailsViewElmItems[index] = null;
                 var taskMain = function (viewGroups) {
-                    viewItems[index] = [elm('h3', words.join(wordsJoiner))];
+                    detailsViewElmItems[index] = [elm('h3', words.join(wordsJoiner))];
+                    paragraphViewArrItems[index] = [];
                     if (viewGroups.length > 0) {
-                        viewItems[index].push.apply(viewItems[index], viewGroups.map(function (rows) {
+                        detailsViewElmItems[index].push.apply(detailsViewElmItems[index], viewGroups.map(function (rows) {
                             return elm('table', rows.map(function (cells) {
                                 return elm('tr', cells.map(function (cell) {
                                     return elm('td', cell !== null ? cell : null_replacement);
                                 }));
                             }));
                         }));
+                        var phrases = viewGroups[0][0];
+                        var phonetics = viewGroups[0][1];
+                        var viPhonetics = viewGroups[0][2];
+                        for (var i = 0; i < phrases.length; i++) {
+                            var null_rep = null_replacement;
+//                            if (/([0-9]|[a-z])*/i.test(phrases[i])) {
+//                                null_rep = phrases[i];
+//                            }
+                            paragraphViewArrItems[index].push([
+                                phrases[i],
+                                phonetics[i] !== null ? phonetics[i] : null_rep,
+                                viPhonetics[i] !== null ? viPhonetics[i] : null_rep
+                            ]);
+                        }
                     } else {
-                        viewItems[index].push.apply(viewItems[index], elm('div', 'Không có dữ liệu', {'class': 'error'}));
+                        detailsViewElmItems[index].push(elm('div', 'Không có dữ liệu', {'class': 'error'}));
+                        words.forEach(function (word) {
+                            var null_rep = null_replacement;
+//                            if (/([0-9]|[a-z])*/i.test(word)) {
+//                                null_rep = phrases[i];
+//                            }
+                            paragraphViewArrItems[index].push([word, null_rep, null_rep]);
+                        });
                     }
                     if (currentHeavyTaskIndex < heavyTasks.length - 1) {
                         currentHeavyTaskIndex++;
                         heavyTasks[currentHeavyTaskIndex]();
                     }
-                    if (viewItems.every(function (viewItem) {
-                            return viewItem !== null;
-                        })) {
-                        empty(result);
-                        viewItems.forEach(function (viewItem) {
-                            appendChildren(result, viewItem);
-                        });
+                    if (detailsViewElmItems.every(function (item) { return item !== null; })) {
+                        renderViews();
                     }
                 };
-
                 heavyTasks.push(function () {
                     if (typeof(Worker) !== "undefined") {
                         var worker = new Worker(chinese_phonetic_worker_src);
@@ -144,17 +202,14 @@ $chinese_text_analyzer_src = Yii::getAlias('@web/js/chinese_text_analyzer.js');
                     }
                 });
             }
-
-            if (heavyTasks.length > 0) {
-                currentHeavyTaskIndex = 0;
-                heavyTasks[currentHeavyTaskIndex]();
-            } else {
-                empty(result);
-                viewItems.forEach(function (viewItem) {
-                    appendChildren(result, viewItem);
-                });
-            }
         });
+
+        if (heavyTasks.length > 0) {
+            currentHeavyTaskIndex = 0;
+            heavyTasks[currentHeavyTaskIndex]();
+        } else {
+            renderViews();
+        }
     }
 
     function handleOnRequestError(error) {
@@ -164,12 +219,18 @@ $chinese_text_analyzer_src = Yii::getAlias('@web/js/chinese_text_analyzer.js');
         ]);
     }
 
-    function requestTranslation(search, onSuccess, onError) {
+    function requestPhoneticApi(search, onSuccess, onError) {
         var webUrl = "<?= Url::to(['lookup-phonetic-for-chinese-text/index', 'search' => '__SEARCH__']) ?>";
         var apiUrl = "<?= Url::to(['chinese-phrase-phonetic-api/lookup', 'wordsList' => '__WORDS__']) ?>";
         search = search.split(' ').join(''); // chinese does not use white space
         window.history.pushState(history.state, document.title,
             webUrl.split("__SEARCH__").join(search));
+        var inputParseResult = ChineseTextAnalyzer.parseChineseText(search);
+        var inputMixedParts = inputParseResult[0];
+        var inputLetterPartIndexes = inputParseResult[1];
+        var wordsList = inputLetterPartIndexes.map(function (letterPartIndex) {
+            return inputMixedParts[letterPartIndex];
+        });
         var xhr = new XMLHttpRequest();
         xhr.addEventListener("load", function () {
             var responseText = xhr.responseText;
@@ -177,15 +238,11 @@ $chinese_text_analyzer_src = Yii::getAlias('@web/js/chinese_text_analyzer.js');
             if (res.error_message) {
                 onError(res.error_message);
             } else {
-                onSuccess(res.data);
+                onSuccess(res.data, inputParseResult);
             }
         });
         xhr.addEventListener("error", function () {
             onError("An error occurred.")
-        });
-        var parseResult = ChineseTextAnalyzer.parseChineseText(search);
-        var wordsList = parseResult[1].map(function (letterPartIndex) {
-            return parseResult[0][letterPartIndex];
         });
         xhr.open("GET", apiUrl.split("__WORDS__").join(JSON.stringify(wordsList)));
         xhr.send();
